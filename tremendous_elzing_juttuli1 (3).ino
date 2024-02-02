c369e52ba1fc0e21aa82471c229d96f80cd21ce7
SAMER MOHAMED ZAID <157971075+SamerZaid20@users.noreply.github.com>
2024-02-02 14:23:49 +0200

    Add files via upload

---
+++ tremendous_elzing_juttuli1 (3).ino
@@ -0,0 +1,50 @@
+// C++ code
+//
+int buttonPin = 5;  
+ int led1Pin = 2;   
+ int led2Pin = 3;   
+ int led3Pin =4;
+  int buttonState = 0;  
+int ledState = 0; 
+void setup()
+{
+  pinMode(buttonPin, INPUT);
+  pinMode(led1Pin, OUTPUT);
+  pinMode(led2Pin, OUTPUT);
+  pinMode(led3Pin, OUTPUT);
+}
+
+void loop()
+{
+ buttonState = digitalRead(buttonPin);
+
+  if (buttonState == HIGH) {  
+    ledState++;              
+
+    if (ledState > 3) {       
+      ledState = 1;
+    }
+
+    if (ledState == 1) {
+      digitalWrite(led1Pin, HIGH);
+      digitalWrite(led2Pin, LOW);
+      digitalWrite(led3Pin, LOW);
+    } else if (ledState == 2) {
+      digitalWrite(led1Pin, HIGH);
+      digitalWrite(led2Pin, HIGH);
+      digitalWrite(led3Pin, LOW);
+    } else if (ledState == 3) {
+      digitalWrite(led1Pin, HIGH);
+      digitalWrite(led2Pin, HIGH);
+      digitalWrite(led3Pin, HIGH);
+    }
+    else {
+       ledState = 0; 
+    digitalWrite(led1Pin, LOW);  
+    digitalWrite(led2Pin, LOW);
+    digitalWrite(led3Pin, LOW);
+  }
+
+    delay(200); 
+  }
+}
